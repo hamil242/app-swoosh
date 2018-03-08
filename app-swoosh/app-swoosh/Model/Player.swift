bc782925a9b3686ecf6a65dad3a1847d4966d4fa
STEVEN L. HAMILTON <shamilto@us.ibm.com>
2018-03-08 16:07:01 -0500

    built model with IBActions

---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Steve Hamilton on 3/8/18.
+//  Copyright © 2018 Steve Hamilton. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
